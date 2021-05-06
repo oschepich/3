dada8510d31a55e0e9614301723d487ff7078259
Alexandr Martynets <amartinets@yandex.ru>
2021-05-06 12:04:45 +0300

    рефактор

--- src/main/java/com/amr/project/dao/abstracts/ReadWriteDao.java
+++ src/main/java/com/amr/project/dao/abstracts/ReadWriteDao.java
@@ -1,6 +1,21 @@
 package com.amr.project.dao.abstracts;
 
-public interface ReadWriteDao {
+import java.util.List;
 
-    void deleteByKeyCascadeIgnore(long id);
+public interface ReadWriteDao<T, K> {
+    void persist(T t);
+
+    void update(T t);
+
+    void delete(T t);
+
+    void deleteByKeyCascadeEnable(K id);
+
+    void deleteByKeyCascadeIgnore(K id);
+
+    boolean existsById(K id);
+
+    T getByKey(K id);
+
+    List<T> getAll();
 }
--- src/main/java/com/amr/project/dao/impl/ReadWriteDaoImpl.java
+++ src/main/java/com/amr/project/dao/impl/ReadWriteDaoImpl.java
@@ -6,20 +6,63 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
-public abstract class ReadWriteDaoImpl<T> implements ReadWriteDao {
+public abstract class ReadWriteDaoImpl<T, K> implements ReadWriteDao<T, K> {
+
     @PersistenceContext
     private EntityManager entityManager;
     private final Class<T> tClass;
+
     @SuppressWarnings("unchecked")
-    public ReadWriteDaoImpl() {
+    protected ReadWriteDaoImpl() {
         tClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
     }
 
     @Override
     @Transactional
-    public void deleteByKeyCascadeIgnore(long id) {
+    public void deleteByKeyCascadeIgnore(K id) {
         entityManager.createQuery("delete from " + tClass.getName() + " t where t.id = :id")
                 .setParameter("id", id).executeUpdate();
+    }
+
+    @Override
+    @Transactional
+    public void persist(T t) {
+        entityManager.persist(t);
+    }
+
+    @Override
+    @Transactional
+    public void update(T t) {
+        entityManager.merge(t);
+    }
+
+    @Override
+    @Transactional
+    public void delete(T t) {
+        entityManager.remove(entityManager.contains(t) ? t : entityManager.merge(t));
+    }
+
+    @Override
+    @Transactional
+    public void deleteByKeyCascadeEnable(K id) {
+        entityManager.remove(entityManager.find(tClass, id));
+    }
+
+    @Override
+    public boolean existsById(K id) {
+        return entityManager.find(tClass, id) != null;
+    }
+
+    @Override
+    public T getByKey(K id) {
+        return entityManager.find(tClass, id);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<T> getAll() {
+        return entityManager.createQuery("from " + tClass.getName()).getResultList();
     }
 }
